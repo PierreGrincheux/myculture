466afda776afea3823b8999999518358be8cea48
AnastasiaProf <anastasia.chinsky@isep.fr>
2016-04-07 15:10:52 +0200

    Ana:integration actual situation

--- Plante/db/schema.rb
+++ Plante/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160331125504) do
+ActiveRecord::Schema.define(version: 20160405194830) do
 
   create_table "control_cards", force: :cascade do |t|
     t.text "serial_nbr", limit: 65535
@@ -44,9 +44,35 @@ ActiveRecord::Schema.define(version: 20160331125504) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.text "login",    limit: 65535
-    t.text "password", limit: 65535
+    t.text     "login",                  limit: 65535
+    t.text     "password",               limit: 65535
+    t.string   "email",                  limit: 255,   default: "",    null: false
+    t.string   "encrypted_password",     limit: 255,   default: "",    null: false
+    t.string   "reset_password_token",   limit: 255
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          limit: 4,     default: 0,     null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
+    t.string   "confirmation_token",     limit: 255
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email",      limit: 255
+    t.integer  "failed_attempts",        limit: 4,     default: 0,     null: false
+    t.string   "unlock_token",           limit: 255
+    t.datetime "locked_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "first_name",             limit: 255
+    t.string   "last_name",              limit: 255
+    t.boolean  "teacher",                              default: false
+    t.boolean  "super_admin",                          default: false
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   create_table "value_types", force: :cascade do |t|
     t.text "name", limit: 65535
