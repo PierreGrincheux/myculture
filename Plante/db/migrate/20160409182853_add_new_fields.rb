df2ec06c1cea2307851f48846ae13e0b7dd32dce
PierreGrincheux <pierre.prunieres@live.fr>
2016-04-14 16:43:08 +0200

    Pierre stuffs

--- Plante/db/migrate/20160409182853_add_new_fields.rb
+++ Plante/db/migrate/20160409182853_add_new_fields.rb
@@ -1,9 +1,9 @@
 class AddNewFields < ActiveRecord::Migration
   def up
-		#add_column :greenhouses, :serial_nbr, :text
+		add_column :greenhouses, :serial_nbr, :text
   end
 	
 	def down
-		#remove_column :greenhouses, :serial_nbr
+		remove_column :greenhouses, :serial_nbr
 	end
 end
