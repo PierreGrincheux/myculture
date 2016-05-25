87e63a5bca29653ef7125fb5ce9c2e067157f595
PierreGrincheux <pierre.prunieres@live.fr>
2016-05-25 17:00:28 +0200

    Pierre : nouveau graphe

--- Plante/app/assets/javascripts/data_show2.js
+++ Plante/app/assets/javascripts/data_show2.js
@@ -22,19 +22,43 @@ for (i = 0; i < arrayIllValue.length; i++){
 	illData.push([Date.parse(arrayIllDate[i]),parseInt(arrayIllValue[i])]);
 }
 
-alert(typeof(illData[0]));
+/*humidité sol*/
+var dataSource5 = $("#soil_humidity-value").attr("data-source");
+var arrayHumSoilValue = dataSource5.split("!");
+
+var dataSource6 = $("#soil_humidity-date").attr("data-source"); 
+var arrayHumSoilDate = dataSource6.split("!");
+
+var humSoilData = new Array();
+for(i = 0; i < arrayHumSoilDate.length; i++){
+	humSoilData.push([Date.parse(arrayHumSoilDate[i]),parseInt(arrayHumSoilValue[i])]);
+}
+
+/*humidité air*/
+var dataSource7 = $("#air_humidity-value").attr("data-source");
+var arrayHumAirValue = dataSource7.split("!");
+
+var dataSource8 = $("#air_humidity-date").attr("data-source"); 
+var arrayHumAirDate = dataSource8.split("!");
+
+var humAirData = new Array();
+for(i = 0; i < arrayHumAirDate.length; i++){
+	humAirData.push([Date.parse(arrayHumAirDate[i]),parseInt(arrayHumAirValue[i])]);
+}
+
+
 
 
 $(function () {
     $('#chart').highcharts({
         chart: {
-            type: 'spline'
+            zoomType: 'xy'
         },
         title: {
-            text: 'Données relevées dans la serre ...'
+            text: 'Données réupérées dans la serre'
         },
         subtitle: {
-            text: '- .... -'
+            text: 'Serre X'
         },
         xAxis: {
             type: 'datetime',
@@ -46,31 +70,118 @@ $(function () {
                 text: 'Date'
             }
         },
-        yAxis: {
-            title: {
-                text: ''
-            },
-            min: 0
-        },
-        tooltip: {
-            headerFormat: '<b>{series.name}</b><br>',
-            pointFormat: '{point.x:%e. %b}: {point.y:.2f} m'
-        },
 
-        plotOptions: {
-            spline: {
-                marker: {
-                    enabled: true
+
+
+
+        yAxis: [{ // Primary yAxis
+            labels: {
+                format: '{value}°C',
+                style: {
+                    color: Highcharts.getOptions().colors[0]
+                }
+            },
+            title: {
+                text: 'Température',
+                style: {
+                    color: Highcharts.getOptions().colors[0]
+                }
+            },
+
+        }, { // Secondary yAxis
+            gridLineWidth: 0,
+            title: {
+                text: 'Éclairement',
+                style: {
+                    color: Highcharts.getOptions().colors[1]
+                }
+            },
+            labels: {
+                format: '{value} lux',
+                style: {
+                    color: Highcharts.getOptions().colors[1]
                 }
             }
+
+        }, { // Tertiary yAxis
+            gridLineWidth: 0,
+            title: {
+                text: 'Humidité du sol',
+                style: {
+                    color: Highcharts.getOptions().colors[2]
+                }
+            },
+            labels: {
+                format: '{value}%',
+                style: {
+                    color: Highcharts.getOptions().colors[2]
+                }
+            },
+            opposite: true
+				}, { // 4th yAxis
+            gridLineWidth: 0,
+            title: {
+                text: 'Humidité de l\'air',
+                style: {
+                    color: Highcharts.getOptions().colors[3]
+                }
+            },
+            labels: {
+                format: '{value}%',
+                style: {
+                    color: Highcharts.getOptions().colors[3]
+                }
+            },
+            opposite: true
+        }],
+        tooltip: {
+            shared: true
+        },
+        
+				
+				
+				legend: {
+            backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'
         },
 
+
+
         series: [{
-            name: 'Température',
-            data: tempData
-        },{
-						name: 'Éclairement',
-						data: illData				
-				}]
+						name: 'Température',
+            type: 'spline',
+            data: tempData,
+            tooltip: {
+                valueSuffix: ' °C'
+            }
+				}, {
+            name: 'Éclairement',
+            type: 'spline',
+            yAxis: 1,
+            data: illData,
+            tooltip: {
+                valueSuffix: ' lux'
+            }
+        }, {
+						name: 'Humidité du sol',
+            type: 'spline',
+            yAxis: 2,
+            data: humSoilData,
+            tooltip: {
+                valueSuffix: '%'
+            }
+        }, {		
+            		
+            name: 'Humidité de l\'air',
+            type: 'spline',
+						yaxis: 3,
+            data: humAirData,
+            tooltip: {
+                valueSuffix: '%'
+            }
+        }]
     });
 });
+
+
+
+
