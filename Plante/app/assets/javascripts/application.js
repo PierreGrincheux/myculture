4cfd2113bfd510a328a4c44cd76229238295debc
AnastasiaProf <anastasia.chinsky@isep.fr>
2016-05-30 15:41:06 +0200

    fullpage css and bordure

--- Plante/app/assets/javascripts/application.js
+++ Plante/app/assets/javascripts/application.js
@@ -29,7 +29,7 @@
 $(document).ready(function (){
 /*fullpage on home page*/
 
-    $(function(){
+   /* $(function(){
       $('#landing .container').fullpage({
         anchors: ['firstPage', 'secondPage', '3rdPage','4thPage','5thPage'],
         navigation: true,
@@ -38,7 +38,7 @@ $(document).ready(function (){
       });
 
     });
-
+*/
  /*timeline*/
 /*$("#demo").rangeCalendar({
 lang: "fr",
@@ -106,5 +106,123 @@ $(document).ready(function(){
     $( ".test" ).click(function() {
     	$( ".info" ).slideToggle();
 	});
+
+/*animate fullpage */
+(function($) {
+  
+  'use strict';
+
+  // variables
+  var $isAnimatedSecond = $('.second .is-animated'),
+      $isAnimatedSecondSingle = $('.second .is-animated__single'),
+      $isAnimatedThird = $('.third .is-animated'),
+      $isAnimatedThirdSingle = $('.third .is-animated__single'),
+      $isAnimatedFourth = $('.fourth .is-animated'),
+      $isAnimatedFourthSingle = $('.fourth .is-animated__single'),
+      $isAnimatedFifth = $('.fifth .is-animated'),
+      $isAnimatedFifthSingle = $('.fifth .is-animated__single'),
+      $isAnimatedSixth = $('.sixth .is-animated'),
+      $isAnimatedSixthSingle = $('.sixth .is-animated__single');
+
+
+  // initialize fullPage
+  $('#fullpage').fullpage({
+  	anchors: ['firstPage', 'secondPage', '3rdPage','4thPage','5thPage', '6thPage'],
+    navigation: true,
+    navigationPosition: 'right',
+    navigationTooltips: ['Accueil', 'Présentation', 'Fonctionnalités', 'Design', 'Application Web', 'Contact'],
+
+    onLeave: function(index, nextIndex, direction) {
+    
+      /**
+      * use the following condition: 
+      *
+      *   if( index == 1 && direction == 'down' ) {
+      *
+      * if you haven't enabled the dot navigation
+      * or you aren't interested in the animations that occur 
+      * when you jump (using the dot navigation) 
+      * from the first section to another sections 
+      */
+      
+      // first animation
+      if( index == 1 && nextIndex == 2 ){ 
+      	$isAnimatedSecond.addClass('animated zoomIn').css('animation-delay', '.6s');
+        $isAnimatedSecondSingle.addClass('animated lightSpeedIn').css('animation-delay', '1.2s');
+        $isAnimatedSecondSingle.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+        $(this).removeClass('lightSpeedIn').addClass('zoomOutDown');
+        });
+        /*$isAnimatedFourth.addClass('animated fadeInUpBig'); 
+        $isAnimatedFourth.eq(0).css('animation-delay', '.3s');
+        $isAnimatedFourth.eq(1).css('animation-delay', '.6s');
+        $isAnimatedFourth.eq(2).css('animation-delay', '.9s');
+        $isAnimatedFourthSingle.addClass('animated rollIn').css('animation-delay', '1.7s');*/
+      }
+
+       else if( (index == 1 || index == 2)  && nextIndex == 3 ){ 
+        $isAnimatedThird.addClass('animated zoomIn').css('animation-delay', '.6s');
+        $isAnimatedThirdSingle.addClass('animated lightSpeedIn').css('animation-delay', '1.2s');
+        $isAnimatedThirdSingle.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+        $(this).removeClass('lightSpeedIn').addClass('zoomOutDown');
+        });
+      }
+
+    /**
+      * use the following condition: 
+      *
+      *   else if( index == 2 && direction == 'down' ) {
+      *
+      * if you haven't enabled the dot navigation
+      * or you aren't interested in the animations that occur 
+      * when you jump (using the dot navigation) from the first section to the third one 
+      */
+      
+      // second animation
+      else if( ( index == 1 || index == 2 || index == 3 ) && nextIndex == 4 ) {
+        $isAnimatedFourth.addClass('animated fadeInUpBig'); 
+        $isAnimatedFourth.eq(0).css('animation-delay', '.3s');
+        $isAnimatedFourth.eq(1).css('animation-delay', '.6s');
+        $isAnimatedFourth.eq(2).css('animation-delay', '.9s');
+        $isAnimatedFourthSingle.addClass('animated rollIn').css('animation-delay', '1.7s');
+      }
+
+      
+     /**
+      * use the following condition:
+      *
+      *   else if( index == 3 && direction == 'down' ) {
+      *
+      * if you haven't enabled the dot navigation
+      * or you aren't interested in the animations that occur 
+      * when you jump (using the dot navigation) 
+      * from the first or second section to the fourth one 
+      */
+      
+      // third animation
+      else if( ( index == 1 || index == 2 || index == 3 || index == 4 ) && nextIndex == 5 ) {
+        $isAnimatedFifth.eq(0).addClass('animated fadeInRightBig').css('animation-delay', '.3s'); 
+        $isAnimatedFifth.eq(1).addClass('animated fadeInLeftBig').css('animation-delay', '.6s');
+        $isAnimatedFifth.eq(2).addClass('animated fadeInUpBig').css('animation-delay', '1.2s');
+        $isAnimatedFifthSingle.addClass('animated bounceInDown').css('animation-delay', '1.6s');
+      }
+
+      //contact
+       else if( ( index == 1 || index == 2 || index == 3 || index == 4 || index == 5) && nextIndex == 6 ) {
+        $isAnimatedSixth.addClass('animated fadeInRightBig'); 
+        $isAnimatedSixth.eq(0).css('animation-delay', '.3s'); 
+        $isAnimatedSixth.eq(1).css('animation-delay', '.6s');
+        $isAnimatedSixth.eq(2).css('animation-delay', '.9s');
+        $isAnimatedSixth.eq(3).css('animation-delay', '1.2s');
+        $isAnimatedSixth.eq(4).css('animation-delay', '1.5s');
+        $isAnimatedSixthSingle.addClass('animated bounceInDown').css('animation-delay', '1.6s');
+      }
+
+
+    }
+
+  });
+  
+})(jQuery);
+
 
 });
